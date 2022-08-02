f75424e5701a3076a83eff3952631110f0cb1eea
Cheesemcpuff <joshuahall98@live.com>
2022-08-02 21:11:38 +0100

    pre URP poly assets and ogham stone script changes

--- Untitled RPG 3D/Assets/Scripts/Josh Test/OghamStoneFloat.cs
+++ Untitled RPG 3D/Assets/Scripts/Josh Test/OghamStoneFloat.cs
@@ -15,22 +15,22 @@ public class OghamStoneFloat : MonoBehaviour
 
     public bool rotated = false;
     public bool startFloating = false;
+    public bool inRange = true;
 
     Vector3 startingPos;
     [SerializeField]Vector3 rotation;
+    Vector3 originalPos;
     [SerializeField]float vecCounter;
+    [SerializeField] float vecCounterCompare;
     public float distance;
 
-    [SerializeField]float maxHeight;
-
-    float midPoint;
-    [SerializeField]float myvar;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        originalPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         currentPosY = transform.position.y;
         height = Random.Range(0.9f, 1.1f);
         distance = Random.Range(0.1f, 0.3f);
@@ -45,8 +45,8 @@ public class OghamStoneFloat : MonoBehaviour
     void Update()
     {
 
-
-        if(rotated == false)
+        //getting the objects to float up
+        if(inRange == true)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(90, 0, 0), turnSpeed * Time.deltaTime);
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, newPosY, transform.position.z), moveSpeed * Time.deltaTime);
@@ -54,47 +54,62 @@ public class OghamStoneFloat : MonoBehaviour
             
         }
         
+        //stop the objects floating up when it gets over a certain point
         if (transform.rotation.eulerAngles.x >= 80)
         {
             rotated = true;
         }
 
-        if(rotated == true )
+        if(rotated == true)
         {
             transform.Rotate(rotation * Time.deltaTime);
 
+
+            //this small snippet below caluclates the sine waves position and commeneces the floating of the objects when the sine wave matches their current position, this prevents stutter
             vecCounter = Mathf.Sin(Time.time * floatSpeed) * distance + startingPos.y;
             vecCounter = Mathf.Round(vecCounter * 100.0f) * 0.01f;
   
-            myvar = Mathf.Round((startingPos.y) * 100.0f) * 0.01f;
+            vecCounterCompare = Mathf.Round((startingPos.y) * 100.0f) * 0.01f;
 
-            if (vecCounter == myvar)
+            if (vecCounter == vecCounterCompare)
             {
                 startFloating = true;
             }
 
-
+            //getting the objects to float up and down following the sine wave
             if (startFloating == true)
             {
                 transform.position = new Vector3(startingPos.x, Mathf.Sin(Time.time * floatSpeed) * distance + startingPos.y, startingPos.z);
                 
             }
+        }
 
-
+        if(inRange == false)
+        {
+            StonesInactive();
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void StonesInactive()
     {
-        //rocks go float
-        //if(collsion == player){
-        //
-        //}
+        rotated = false;
+        transform.position = originalPos;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            inRange = true;
+        }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        //rocks stop float
+        if (collision.gameObject.tag == "Player")
+        {
+            inRange = false;
+        }
     }
 
 }
